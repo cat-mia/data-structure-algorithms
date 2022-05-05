a6e28669a1f3da8ecfac09dc4a5de161d0347f95
cat-mia <limingxia1999@gmail.com>
2022-05-05 19:49:39 +0800

    greedy for max activity

--- Greedy/max_activity.py
+++ Greedy/max_activity.py
@@ -1,3 +1,14 @@
 # 一个会议室，n个活动，每个活动有开始时间和结束时间
 # 返回会议室最多能办多少场活动
 
+# 最先安排最早结束的活动
+
+def max_activity(time_arr):
+    time_arr.sort(key=lambda x:x[1])
+    ans = 0
+    cur_t = 0
+    for t in time_arr:
+        if t[0] >= cur_t:
+            ans += 1
+            cur_t = t[1]
+    return ans
