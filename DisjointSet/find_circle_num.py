8bf9e074ecb6c06446695ede995ae11b90bcf4c1
cat-mia <limingxia1999@gmail.com>
2022-05-08 14:57:16 +0800

    leetcode 547

--- DisjointSet/find_circle_num.py
+++ DisjointSet/find_circle_num.py
@@ -39,3 +39,57 @@ class Solution:
             s.add(self.find(i))
         return len(s)
         
+# or 复用代码
+class Node:
+    def __init__(self, val) -> None:
+        self.val = val
+
+class DisjointSet:
+    def __init__(self, data_list) -> None:
+        self.size_map = {} # 只存每个集合头节点
+        self.father = {}
+        self.nodes = {}
+        self.add(data_list)
+
+    def add(self, data_list):
+        for v in data_list:
+            node = Node(v)
+            self.nodes[v] = node
+            self.father[node] = node
+            self.size_map[node] = 1
+
+    def ancestor(self, node):
+        path = []
+        cur = node
+        while cur != self.father[cur]:
+            path.append(cur)
+            cur = self.father[cur]
+        for n in path:
+            self.father[n] = cur
+        return cur
+
+    # 小的集合挂到大的集合上
+    def union(self, x, y):
+        x_a = self.ancestor(self.nodes[x])
+        y_a = self.ancestor(self.nodes[y])
+        if x_a == y_a:
+            return
+        small = x_a if self.size_map[x_a] > self.size_map[y_a] else y_a
+        big = y_a if self.size_map[x_a] > self.size_map[y_a] else x_a
+        self.father[small] = big
+        self.size_map[big] = self.size_map[small] + self.size_map[big]
+        del self.size_map[small]
+
+class Solution:
+    def findCircleNum(self, isConnected) -> int:
+        n = len(isConnected)
+        s = DisjointSet([_ for _ in range(n)])
+        # 只遍历半个区域
+        for i in range(n):
+            for j in range(i+1, n):
+                if isConnected[i][j] == 1:
+                    s.union(i, j)
+        return len(s.size_map)
+        
+s = Solution()
+print(s.findCircleNum([[1,1,1],[1,1,1],[1,1,1]]))
