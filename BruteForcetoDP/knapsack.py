fc6f75bfca765cb83ba03213369da140a690022b
cat-mia <limingxia1999@gmail.com>
2022-06-01 16:19:44 +0800

    2D knapsack

--- BruteForcetoDP/knapsack.py
+++ BruteForcetoDP/knapsack.py
@@ -18,4 +18,22 @@ def process(w, v, index, rest):
 
 print(process([5,5,3], [1,2,9], 0, 10))
 
-# def dp_knapsack(w, v, capacity):
+# 暴搜改2D-dynamic programming
+# 有重复计算的问题才有必要改dp
+# dp[index][rest]
+# 0~index已经放好
+# index 0~N  rest 0~capacity
+# w weight  v value
+def dp_knapsack(w, v, capacity):
+    dp = [[0]*(capacity+1) for _ in range(len(w)+1)]
+    # dp[n][...] = 0
+    for index in range(len(w)-2,-1,-1):
+        for rest in range(0, capacity+1):
+            if rest-w[index+1] >= 0:
+                dp[index][rest] = max(dp[index+1][rest], dp[index+1][rest-w[index+1]]+v[index+1])
+            else:
+                dp[index][rest] = dp[index+1][rest]
+    print(dp)
+    return dp[0][capacity]
+
+print(dp_knapsack([5,5,3], [1,2,9], 10))
